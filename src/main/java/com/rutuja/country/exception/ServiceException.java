6e2fc891d154d65945e7ac777f09540686d25ff6
rutujadattajadhav@gmail.com <rutujadattajadhav@gmail.com>
2025-01-17 10:52:31 +0000

    splunk working

--- src/main/java/com/rutuja/country/exception/ServiceException.java
+++ src/main/java/com/rutuja/country/exception/ServiceException.java
@@ -7,11 +7,35 @@ import reactor.core.publisher.Flux;
 import java.util.List;
 
 
-@AllArgsConstructor
+
 public class ServiceException extends Exception {
 List<Error> errorMessage;
 
     public List<Error> getErrorMessage() {
         return errorMessage;
+    }
+
+    public ServiceException(List<Error> errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public ServiceException(String message, List<Error> errorMessage) {
+        super(message);
+        this.errorMessage = errorMessage;
+    }
+
+    public ServiceException(String message, Throwable cause, List<Error> errorMessage) {
+        super(message, cause);
+        this.errorMessage = errorMessage;
+    }
+
+    public ServiceException(Throwable cause, List<Error> errorMessage) {
+        super(cause);
+        this.errorMessage = errorMessage;
+    }
+
+    public ServiceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace, List<Error> errorMessage) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.errorMessage = errorMessage;
     }
 }
