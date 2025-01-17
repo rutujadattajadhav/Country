70d0d16790eaf0b957c67702ef72fbc1411eb070
rutujadattajadhav <rutujadattajadhav@gmail.com>
2025-01-17 14:06:22 +0530

    splunk configuration

--- src/main/java/com/rutuja/country/CountryApplication.java
+++ src/main/java/com/rutuja/country/CountryApplication.java
@@ -1,19 +1,22 @@
 package com.rutuja.country;
 
+import org.apache.logging.log4j.ThreadContext;
 import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import reactor.core.publisher.Hooks;
 
 import java.util.UUID;
 
 @SpringBootApplication
+@ComponentScan("com.rutuja")
 public class CountryApplication {
 
 	public static void main(String[] args) {
-		MDC.put("correlationId", UUID.randomUUID().toString()+"_app_startup_log");
-		Hooks.enableAutomaticContextPropagation();
+		ThreadContext.put("correlationId", UUID.randomUUID().toString()+"spunkdemo_app_startup_log");
 		SpringApplication.run(CountryApplication.class, args);
+		ThreadContext.clearMap();
 	}
 
 }
